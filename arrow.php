c34c2c4f30756c7e7d01464ac627b0ff0a45d012
devdkartik <96041201+devdkartik@users.noreply.github.com>
2022-05-24 21:23:00 +0530

    Delete arrow.php

--- arrow.php
+++
@@ -1,3 +0,0 @@
-
-  <button type="button" id="scrollTop" style="font-size:3rem; font-size: 3rem;background: #f0f8ff00;border: none;cursor: pointer;border-radius: 26%;position: fixed;
-    top: 80%;right: 0;">&uarr;</button>
